e6646a954612409544a6434c82347d9ac2595b49
Emre BALCI <emre-balci@outlook.com.tr>
2023-02-19 23:17:14 +0300

    Create repository for Book table

---
+++ src/main/java/eballci/bookstore/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package eballci.bookstore.repository;
+
+import eballci.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
